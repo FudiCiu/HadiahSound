25c442c7605ac5b7747e63bef791a1c06d9ad7d5
FudiCiu <fudiantociuandi_17@kharisma.ac.id>
2019-07-03 22:16:09 +0800

    add hadiah sound

---
+++ HadiahSound/app/src/main/java/com/latihanandroid/hadiahsound/Main2Activity.java
@@ -0,0 +1,21 @@
+package com.latihanandroid.hadiahsound;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        MainActivity.playSound(this,2);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        MainActivity.stopSound(null);
+    }
+
+}
---
+++ HadiahSound/app/src/main/java/com/latihanandroid/hadiahsound/MainActivity.java
@@ -0,0 +1,62 @@
+package com.latihanandroid.hadiahsound;
+
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public static MediaPlayer mediaPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        playSound(this,1);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopSound(null);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        playSound(this,1);
+    }
+
+    public static  void stopSound(@Nullable View view){
+        mediaPlayer.stop();
+        mediaPlayer.release();
+    }
+    public static void playSound(Context context, int i) {
+        try {
+            if (mediaPlayer.isPlaying()){
+                mediaPlayer.stop();
+                mediaPlayer.release();
+            }
+        } catch (Exception e){
+            Toast.makeText(context,"Masuk eksepsion",Toast.LENGTH_SHORT).show();
+        }
+
+        if (i==1){
+            Toast.makeText(context,"Sedang main lagu1",Toast.LENGTH_SHORT).show();
+            mediaPlayer= MediaPlayer.create(context,R.raw.hbd);
+        }else if (i==2){
+            Toast.makeText(context,"Sedang main lagu2",Toast.LENGTH_SHORT).show();
+            mediaPlayer=MediaPlayer.create(context,R.raw.hbd2);
+        }
+        mediaPlayer.setLooping(false);
+        mediaPlayer.start();
+    }
+
+    public void Gantiform(View view) {
+        Intent intent= new Intent(MainActivity.this,Main2Activity.class);
+        startActivity(intent);
+    }
+}
